1ee4647df5861365a2e11fb1960abbdec6878379
linda <linda@sina.com>
2019-10-08 15:04:00 +0800

    '分页'

--- js/news.js
+++ js/news.js
@@ -1,12 +1,27 @@
-$.get("http://192.168.20.18:1800/api/v1/company/news/queryAllByPage?categoryTypeId=2", function (res) {
-    //$("div").html(result);
-    if (res.statusCode == 200) {
-        var data = res.data;
-        var htmlStr = '';
-        var item;
-        for (var i = 0; i < data.list.length; ++i) {
-            item = data.list[i];
-            htmlStr += `<li class="news_item row ">
+// 初始化加载
+
+var pageMap = {}
+var nowTypeId = 2;
+var nowIndex = 0;
+var pageSize = 10;
+
+init()
+
+function init() {
+    click();
+    pageMap["categoryId_" + nowTypeId] = 1;
+    nowIndex = 0;
+    loadData(renderHtml)
+}
+
+// 渲染页面
+function renderHtml(res) {
+    var data = res.data;
+    var htmlStr = '';
+    var item;
+    for (var i = 0; i < data.list.length; ++i) {
+        item = data.list[i];
+        htmlStr += `<li class="news_item row ">
                                 <div class="col-md-4 item_left">
                                     <img src="${item.listFiles}">
                                 </div>
@@ -21,75 +36,84 @@ $.get("http://192.168.20.18:1800/api/v1/company/news/queryAllByPage?categoryType
                                     </div>
                                 </div>
                             </li>`
+    }
+    $("#v-pills-tabContent .tab-pane").eq(nowIndex).find(".am-content").html(htmlStr)
+    readerNavigation(res)
+}
+
+// 导航渲染
+function readerNavigation(res) {
+    var data = res.data;
+    var navData = data.navigatepageNums;
+    var str = '';
+    for (var i = 0; i < navData.length; ++i) {
+        if (navData[i] === data.pageNum) {
+            str += '<li class="page-item active"><a class="page-link am-link" href="#">' + navData[i] + '</a></li>'
+        } else {
+            str += '<li class="page-item"><a class="page-link am-link" href="#">' + navData[i] + '</a></li>'
         }
-        $('#news_company').html(htmlStr);
-    } else {
-        alert(res.message)
     }
-    console.log(res)
-});
-$.get("http://192.168.20.18:1800/api/v1/company/news/queryAllByPage?categoryTypeId=3", function (res) {
-    //$("div").html(result);
+    var firstStr = $("#firstLi").get(0).outerHTML;
+    var lastStr = $("#lastLi").get(0).outerHTML;
+    var allStr = firstStr + str + lastStr;
+    $('#navUl').html(allStr)
+    navAction(res);
+}
 
-    if (res.statusCode == 200) {
-        var data = res.data;
-        var htmlStr = '';
-        var item;
-        for (var i = 0; i < data.list.length; ++i) {
-            item = data.list[i];
-            htmlStr += `<li class="news_item row ">
-                                <div class="col-md-4 item_left">
-                                    <img src="${item.listFiles}">
-                                </div>
-                                <div class="col-md-8 item_right">
-                                    <div class="item_title">
-                                        <span><a href="details.html?id=${item.id}">${item.newsTittle}</a></span>
-                                        <span class="news_date">${item.createDate}</span>
-                                    </div>
-                                    <div class="item_con"><span>${item.newsConclusion}</span></div>
-                                    <div class="item_more">
-                                        <a href="details.html?id=${item.id}">查看更多>></a>
-                                    </div>
-                                </div>
-                            </li>`
-        };
-
-        $('#news_industry').html(htmlStr);
+/**
+ * 分页添加事件
+ */
+function navAction(res) {
+    var data = res.data;
+    var pageNum = pageMap['categoryId_' + nowTypeId];
+    var privious = $("#previous");
+    var next = $("#next");
+    if (!data.hasPreviousPage) {
+        privious.addClass('disabled')
     } else {
-        alert(res.message)
+        privious.removeClass('disabled')
+        privious.click(function () {
+            pageMap['categoryId_' + nowTypeId] = pageNum - 1;
+            loadData(renderHtml)
+        })
     }
-});
-$.get("http://192.168.20.18:1800/api/v1/company/news/queryAllByPage?categoryTypeId=4", function (res) {
-    //$("div").html(result);
-
-    if (res.statusCode == 200) {
-        var data = res.data;
-        var htmlStr = '';
-        var item;
-        for (var i = 0; i < data.list.length; ++i) {
-            item = data.list[i]
-            htmlStr += `<li class="news_item row ">
-                                <div class="col-md-4 item_left">
-                                    <img src="${item.listFiles}">
-                                </div>
-                                <div class="col-md-8 item_right">
-                                    <div class="item_title">
-                                        <span><a href="details.html?id=${item.id}">${item.newsTittle}</a></span>
-                                        <span class="news_date">${item.createDate}</span>
-                                    </div>
-                                    <div class="item_con"><span>${item.newsConclusion}</span></div>
-                                    <div class="item_more">
-                                        <a href="details.html?id=${item.id}">查看更多>></a>
-                                    </div>
-                                </div>
-                            </li>`
-        }
-
-        $('#news_media').html(htmlStr);
+    if (!data.hasNextPage) {
+        next.addClass('disabled')
     } else {
-        alert(res.message)
+        next.removeClass('disabled')
+        next.click(function () {
+            pageMap['categoryId_' + nowTypeId] = pageNum + 1;
+            loadData(renderHtml)
+        })
     }
-    console.log(res)
-});
+
+    $('.am-link').click(function () {
+        var pageNo = parseInt($(this).html());
+        pageMap['categoryId_' + nowTypeId] = pageNo;
+        loadData(renderHtml);
+    })
+
+}
+
+/**
+ * 左边选项卡点击
+ */
+function click() {
+    $('#left_a a').click(function () {
+        var dataid = $(this).attr('data-id');
+        nowIndex = $(this).index();
+        if (!pageMap['categoryId_' + dataid]) pageMap['categoryId_' + dataid] = 1;
+        console.log(pageMap)
+        nowTypeId = dataid;
+        var str =$("#v-pills-tabContent .tab-pane").eq(nowIndex).find(".am-content").html();
+        console.log(str)
+        loadData(renderHtml)
+    })
+}
 
 
+// 加载分页数据
+function loadData(fnSuccess) {
+    var pageNum = pageMap["categoryId_" + nowTypeId];
+    $.get("http://192.168.20.18:1800/api/v1/company/news/queryAllByPage?categoryTypeId=" + nowTypeId + "&pageSize=" + pageSize + "&pageNum=" + pageNum, fnSuccess)
+}
