6b6533381e38b042e54740a689ee8aa09b63a75e
linda <linda@sina.com>
2019-10-11 17:52:30 +0800

    '新闻分页完善'

--- js/news.js
+++ js/news.js
@@ -1,7 +1,7 @@
 var pageMap = {};
 var nowTypeId = 2;
 var nowIndex = 0;
-var pageSize = 5;
+var pageSize = 1;
 
 init();
 
@@ -19,9 +19,9 @@ function renderHtml(res) {
     var item;
     for (var i = 0; i < data.list.length; ++i) {
         item = data.list[i];
-        if(item.listFiles == ""){
+        if (item.listFiles == "") {
             var imgUrl = item.listFiles = "img/em_news_page/default.png";
-            $("#image").attr("src",imgUrl);
+            $("#image").attr("src", imgUrl);
         }
         htmlStr += `<li class="news_item row ">
                                 <div class="col-md-4 item_left">
@@ -55,9 +55,11 @@ function readerNavigation(res) {
             str += '<li class="page-item pages"><a class="page-link am-link" href="#">' + navData[i] + '</a></li>'
         }
     }
+    var firstPage = $("#firstPage").get(0).outerHTML;
+    var lastPage = $("#lastPage").get(0).outerHTML;
     var firstStr = $("#firstLi").get(0).outerHTML;
     var lastStr = $("#lastLi").get(0).outerHTML;
-    var allStr = firstStr + str + lastStr;
+    var allStr = firstPage + firstStr + str + lastStr + lastPage;
     $('#navUl').html(allStr);
     navAction(res);
 }
@@ -69,28 +71,44 @@ function navAction(res) {
     var data = res.data;
     var pageNum = pageMap['categoryId_' + nowTypeId];
     var privious = $("#previous");
+    var first = $("#first");
+    var last = $("#last");
     var next = $("#next");
     if (!data.hasPreviousPage) {
-        privious.addClass('disabled')
+        privious.addClass('disabled');
+        first.addClass('disabled');
     } else {
-        privious.removeClass('disabled')
+        privious.removeClass('disabled');
+        first.removeClass('disabled');
         privious.click(function () {
             pageMap['categoryId_' + nowTypeId] = pageNum - 1;
-            loadData(renderHtml)
+            loadData(renderHtml);
+        })
+        first.click(function () {
+            pageMap['categoryId_' + nowTypeId] = pageNum = 1;
+            loadData(renderHtml);
         })
     }
     if (!data.hasNextPage) {
         next.addClass('disabled');
+        last.addClass('disabled');
     } else {
         next.removeClass('disabled');
+        last.removeClass('disabled');
         next.click(function () {
             pageMap['categoryId_' + nowTypeId] = pageNum + 1;
+            loadData(renderHtml);
+        })
+        last.click(function () {
+            const totalPage = parseInt(data.total);
+            pageMap['categoryId_' + nowTypeId] = totalPage;
             loadData(renderHtml);
         })
     }
 
     $('.am-link').click(function () {
         var pageNo = parseInt($(this).html());
+        console.log($(this).html());
         pageMap['categoryId_' + nowTypeId] = pageNo;
         loadData(renderHtml);
     })
@@ -107,7 +125,7 @@ function click() {
         if (!pageMap['categoryId_' + dataid]) pageMap['categoryId_' + dataid] = 1;
         console.log(pageMap);
         nowTypeId = dataid;
-        var str =$("#v-pills-tabContent .tab-pane").eq(nowIndex).find(".am-content").html();
+        var str = $("#v-pills-tabContent .tab-pane").eq(nowIndex).find(".am-content").html();
         loadData(renderHtml);
     })
 }
@@ -116,38 +134,9 @@ function click() {
 * 限制分页显示数目
 *
 * */
+
 // 加载分页数据
 function loadData(fnSuccess) {
     var pageNum = pageMap["categoryId_" + nowTypeId];
     $.get("http://192.168.20.18:1800/api/v1/company/news/queryAllByPage?categoryTypeId=" + nowTypeId + "&pageSize=" + pageSize + "&pageNum=" + pageNum, fnSuccess)
-}
-
-
-/*判断图片是否存在*/
-var CheckImgExists = function CheckImg(imgurl) {
-    return new Promise(function(resolve, reject) {
-        var ImgObj = new Image();
-        ImgObj.onload=function(){
-             console.log(ImgObj.width+"==",ImgObj.height+"==");
-            resolve("load image success");
-        }
-        ImgObj.onerror=function(){
-             console.log('error');
-            reject('Could not load image at ' + imgurl);
-        }
-        ImgObj.src = imgurl;
-    });
-}
-
-/*图片不存在则使用默认图片*/
-function setImage(imgurl){
-    //图片存在执行第一个函数，否则执行第二个函数
-    CheckImgExists(imgurl).then(function(json) {
-        $("#image").attr("src",imgurl);
-        console.log(json);
-    }, function(error) {
-        $("#image").attr("src",'');
-        console.error(error);
-    });
-
 }
